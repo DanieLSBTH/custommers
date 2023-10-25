6ecd074700e296294db567cfb7b364d30b61e672
DanieLSBTH <josuemarque15@gmail.com>
2023-10-25 09:52:07 -0600

    dos_commit

--- app/config/env.js
+++ app/config/env.js
@@ -1,7 +1,7 @@
 const env = {
   database: 'umg_antigua',
   username: 'umg_antigua_user',
-  password: '6mQANExZDR0XSp5fsB5LG7m14Ldz2o6@',
+  password: '6mQANExZDR0XSp5fsB5LG7m14Ldz2o6',
   host: 'dpg-ckrfeqm2eoec73ffjql0-a.oregon-postgres.render.com',
   dialect: 'postgres',
   pool: {
