fa4a38880e610c4b73ec6c7c36e093b4a53aa3ed
DanieLSBTH <josuemarque15@gmail.com>
2023-10-25 00:46:42 -0600

    commit

---
+++ app/config/env.js
@@ -0,0 +1,17 @@
+
+
+const env = {
+  database: 'umg_antigua',
+  username: 'umg_antigua_user',
+  password: '6mQANExZDR0XSp5fsB5LG7m14Ldz2o6@',
+  host: 'dpg-ckrfeqm2eoec73ffjql0-a.oregon-postgres.render.com',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+module.exports = env;
